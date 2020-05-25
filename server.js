64ec65d5668ffc255fe0d576988bd06652896471
ΓΙΑΝΝΗΣ ΚΑΛΑΪΤΖΑΚΗΣ <yannis.kalaijakis@gmail.com>
2020-05-25 21:06:53 +0300

    add dynamic GET route

--- server.js
+++ server.js
@@ -6,11 +6,14 @@ const mockUserData=[
 	{name:'Jill'}
 ]
 
-app.get('/users',function(req,res){
+
+
+app.get('/users/:id',function(req,res){
+	console.log(req.params.id)
 	res.json({
 		success: true,
-		message: 'successfully got users. Nice!',
-		users: mockUserData
+		message: 'got one user',
+		user: req.params.id
 	})
 })
 
